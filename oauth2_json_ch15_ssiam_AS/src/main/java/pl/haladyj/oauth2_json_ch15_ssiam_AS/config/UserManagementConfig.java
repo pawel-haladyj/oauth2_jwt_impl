37f0c4ddfc4198c098dc4dc0dc0eda9d8e61d5bc
Paweł Haładyj <pawel@haladyj.pl>
2020-06-03 09:08:58 +0200

    Auth Server - opaque token

---
+++ oauth2_json_ch15_ssiam_AS/src/main/java/pl/haladyj/oauth2_json_ch15_ssiam_AS/config/UserManagementConfig.java
@@ -0,0 +1,41 @@
+package pl.haladyj.oauth2_json_ch15_ssiam_AS.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class UserManagementConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        var uds = new InMemoryUserDetailsManager();
+
+        var user = User
+                .withUsername("john")
+                .password("12345")
+                .authorities("read")
+                .build();
+
+        uds.createUser(user);
+
+        return uds;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+}
