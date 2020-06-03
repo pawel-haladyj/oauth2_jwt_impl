143d5c1b2a03180bfa7d682bf153f356982ab751
Paweł Haładyj <pawel@haladyj.pl>
2020-06-03 08:48:24 +0200

    initial commit

---
+++ oauth2_json_ch15_ssiam_AS/src/main/java/pl/haladyj/oauth2_json_ch15_ssiam_AS/Oauth2JsonCh15SsiamAsApplication.java
@@ -0,0 +1,13 @@
+package pl.haladyj.oauth2_json_ch15_ssiam_AS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2JsonCh15SsiamAsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2JsonCh15SsiamAsApplication.class, args);
+	}
+
+}
